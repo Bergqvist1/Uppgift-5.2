1641366418357839c90c000d1f2dae5f31ceac71
Bergqvist1 <sibe05001@utb.vaxjo.se>
2023-01-13 11:08:42 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in antalet städer du har besökt");
+            int antal = int.Parse(Console.ReadLine());
+            string[] stad= new string[antal];
+            
+
+            for(int i=0; i<antal; i++)
+            {
+                Console.WriteLine("Skriv de städer du besökt:");
+                stad[i]= Console.ReadLine();
+                
+            }
+
+            Console.Write("Du har varit i städerna");
+            for(int i=0; i<antal; i++)
+            {
+                Console.Write(" " + stad[i]);
+            }
+        }
+
+    }
+}
